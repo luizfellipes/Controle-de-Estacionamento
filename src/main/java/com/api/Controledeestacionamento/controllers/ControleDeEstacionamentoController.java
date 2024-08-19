ec7a8b62256ee0f38e960b9538d9ffaa4aaeee97
luizfellipes <luiz.13.silva@gmail.com>
2024-08-19 16:33:34 -0400

    refatorado toda a controller e service

--- src/main/java/com/api/Controledeestacionamento/controllers/ControleDeEstacionamentoController.java
+++ src/main/java/com/api/Controledeestacionamento/controllers/ControleDeEstacionamentoController.java
@@ -3,17 +3,12 @@ package com.api.Controledeestacionamento.controllers;
 import com.api.Controledeestacionamento.dtos.ControleDeEstacionamentoDto;
 import com.api.Controledeestacionamento.models.ControleDeEstacionamentoModel;
 import com.api.Controledeestacionamento.services.ControleDeEstacionamentoService;
-import jakarta.persistence.GeneratedValue;
 import jakarta.validation.Valid;
-import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -26,23 +21,9 @@ public class ControleDeEstacionamentoController {
         this.controleDeEstacionamentoService = controleDeEstacionamentoService;
     }
 
-    ControleDeEstacionamentoModel controleDeEstacionamentoModel = new ControleDeEstacionamentoModel();
-
     @PostMapping
     public ResponseEntity<Object> salvarControleDeEstacionamento(@RequestBody @Valid ControleDeEstacionamentoDto controleDeEstacionamentoDto) {
-        if (controleDeEstacionamentoService.existePlacaDeCarro(controleDeEstacionamentoDto.getPlacaDoCarro())) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflito: Já existe um carro com essa placa !");
-        }
-        if (controleDeEstacionamentoService.existeVagaDeCarroEmUso(controleDeEstacionamentoDto.getNumeroDoControleDeEstacionamento())) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflito: A vaga esta em uso !");
-        }
-        if (controleDeEstacionamentoService.existeApartamentoBlocoEmUso(controleDeEstacionamentoDto.getApartamento(), controleDeEstacionamentoDto.getBloco())) {
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflito: A vaga já esta em uso pelo apartamento:  "
-                    + controleDeEstacionamentoDto.getApartamento() + "E bloco: " + controleDeEstacionamentoDto.getBloco());
-        }
-        BeanUtils.copyProperties(controleDeEstacionamentoDto, controleDeEstacionamentoModel);
-        controleDeEstacionamentoModel.setDataDeRegistro(LocalDateTime.now(ZoneId.of("UTC")));
-        return ResponseEntity.status(HttpStatus.CREATED).body(controleDeEstacionamentoService.save(controleDeEstacionamentoModel));
+        return ResponseEntity.status(HttpStatus.CREATED).body(controleDeEstacionamentoService.save(controleDeEstacionamentoDto));
     }
 
     @GetMapping
@@ -52,31 +33,17 @@ public class ControleDeEstacionamentoController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Object> getUmaVaga(@PathVariable(value = "id") UUID id) {
-        Optional<ControleDeEstacionamentoModel> controleDeEstacionamentoModelOptional = controleDeEstacionamentoService.findById(id);
-        return !controleDeEstacionamentoModelOptional.isPresent() ? ResponseEntity.status(HttpStatus.NOT_FOUND).body("Vaga de estacionamento não encontrada !")
-                : ResponseEntity.status(HttpStatus.OK).body(controleDeEstacionamentoModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body(controleDeEstacionamentoService.findById(id));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteVagaDeEstacionamento(@PathVariable(value = "id") UUID id) {
-        Optional<ControleDeEstacionamentoModel> controleDeEstacionamentoModelOptional = controleDeEstacionamentoService.findById(id);
-        if (!controleDeEstacionamentoModelOptional.isPresent()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Vaga de estacionamento não encontrada !");
-        } else {
-            controleDeEstacionamentoService.delete(controleDeEstacionamentoModelOptional.get());
-            return ResponseEntity.status(HttpStatus.OK).body("Vaga de estacionamento deletada com sucesso !");
-        }
+        controleDeEstacionamentoService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Vaga de estacionamento deletada com sucesso !");
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Object> updateVagaDeEstacionamento(@PathVariable(value = "id") UUID id, @RequestBody @Valid ControleDeEstacionamentoDto controleDeEstacionamentoDto){
-        Optional<ControleDeEstacionamentoModel> controleDeEstacionamentoModelOptional = controleDeEstacionamentoService.findById(id);
-        if(!controleDeEstacionamentoModelOptional.isPresent()){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Vaga não encontrada !");
-        }
-        BeanUtils.copyProperties(controleDeEstacionamentoDto, controleDeEstacionamentoModel);
-        controleDeEstacionamentoModel.setId(controleDeEstacionamentoModelOptional.get().getId());
-        controleDeEstacionamentoModel.setDataDeRegistro(controleDeEstacionamentoModelOptional.get().getDataDeRegistro());
-        return ResponseEntity.status(HttpStatus.OK).body(controleDeEstacionamentoService.save(controleDeEstacionamentoModel));
+    public ResponseEntity<Object> updateVagaDeEstacionamento(@PathVariable(value = "id") UUID id, @RequestBody @Valid ControleDeEstacionamentoDto controleDeEstacionamentoDto) {
+         return ResponseEntity.status(HttpStatus.OK).body(controleDeEstacionamentoService.update(id, controleDeEstacionamentoDto));
     }
 }
--- src/main/java/com/api/Controledeestacionamento/services/ControleDeEstacionamentoService.java
+++ src/main/java/com/api/Controledeestacionamento/services/ControleDeEstacionamentoService.java
@@ -1,14 +1,16 @@
 package com.api.Controledeestacionamento.services;
 
+import com.api.Controledeestacionamento.dtos.ControleDeEstacionamentoDto;
 import com.api.Controledeestacionamento.models.ControleDeEstacionamentoModel;
 import com.api.Controledeestacionamento.repositories.ControleDeEstacionamentoRepository;
 import jakarta.transaction.Transactional;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 @Service
 public class ControleDeEstacionamentoService {
@@ -20,8 +22,22 @@ public class ControleDeEstacionamentoService {
     }
 
     @Transactional
-    public ControleDeEstacionamentoModel save(ControleDeEstacionamentoModel controleDeEstacionamentoModel) {
-        return controleDeEstacionamentoRepository.save(controleDeEstacionamentoModel);
+    public ControleDeEstacionamentoModel save(ControleDeEstacionamentoDto controleDeEstacionamentoDto) {
+        return Stream.of(convertControleEstacionamentoDTO(controleDeEstacionamentoDto))
+                .map(controleDeEstacionamentoModel -> {
+                    if (existePlacaDeCarro(controleDeEstacionamentoModel.getPlacaDoCarro())) {
+                        throw new RuntimeException("Conflito: Já existe um carro com essa placa !");
+                    }
+                    if (existeVagaDeCarroEmUso(controleDeEstacionamentoModel.getNumeroDoControleDeEstacionamento())) {
+                        throw new RuntimeException("Conflito: A vaga esta em uso !");
+                    }
+                    if (existeApartamentoBlocoEmUso(controleDeEstacionamentoModel.getApartamento(), controleDeEstacionamentoModel.getBloco())) {
+                        throw new RuntimeException("Conflito: A vaga já esta em uso pelo apartamento:  " + controleDeEstacionamentoModel.getApartamento() + "E bloco: " + controleDeEstacionamentoModel.getBloco());
+                    }
+                    return controleDeEstacionamentoRepository.save(controleDeEstacionamentoModel);
+                })
+                .findFirst()
+                .orElseThrow();
     }
 
     public List<ControleDeEstacionamentoModel> findAll() {
@@ -29,25 +45,35 @@ public class ControleDeEstacionamentoService {
     }
 
     public Optional<ControleDeEstacionamentoModel> findById(UUID id) {
-        return controleDeEstacionamentoRepository.findById(id);
+        return Optional.ofNullable(controleDeEstacionamentoRepository.findById(id).orElseThrow(() -> new RuntimeException("Vaga de estacionamento não encontrada !")));
     }
 
     @Transactional
-    public void delete(ControleDeEstacionamentoModel controleDeEstacionamentoModel) {
-        controleDeEstacionamentoRepository.delete(controleDeEstacionamentoModel);
+    public void deleteById(UUID id) {
+        findById(id).ifPresent(controleDeEstacionamentoRepository::delete);
     }
 
+    public ControleDeEstacionamentoModel update(UUID id, ControleDeEstacionamentoDto controleDeEstacionamentoDto) {
+        Optional<ControleDeEstacionamentoModel> controleDeEstacionamentoModelOptional = findById(id);
+        BeanUtils.copyProperties(convertControleEstacionamentoDTO(controleDeEstacionamentoDto), controleDeEstacionamentoModelOptional.get());
+        return controleDeEstacionamentoRepository.save(controleDeEstacionamentoModelOptional.get());
+    }
 
-    public boolean existePlacaDeCarro(String placaDoCarro) {
+    private boolean existePlacaDeCarro(String placaDoCarro) {
         return controleDeEstacionamentoRepository.existsByPlacaDoCarro(placaDoCarro);
     }
 
-    public boolean existeVagaDeCarroEmUso(String numeroDoControleDeEstacionamento) {
+    private boolean existeVagaDeCarroEmUso(Integer numeroDoControleDeEstacionamento) {
         return controleDeEstacionamentoRepository.existsByNumeroDoControleDeEstacionamento(numeroDoControleDeEstacionamento);
     }
 
-    public boolean existeApartamentoBlocoEmUso(String apartamento, String bloco) {
+    private boolean existeApartamentoBlocoEmUso(String apartamento, String bloco) {
         return controleDeEstacionamentoRepository.existsByApartamentoAndBloco(apartamento, bloco);
     }
+
+    private ControleDeEstacionamentoModel convertControleEstacionamentoDTO(ControleDeEstacionamentoDto DTO) {
+        return new ControleDeEstacionamentoModel(DTO.numeroDoControleDeEstacionamento(), DTO.placaDoCarro(), DTO.marcaDoCarro(), DTO.modeloDoCarro(), DTO.corDoCarro(), DTO.dataDeRegistro(), DTO.nomeDoResponsavel(), DTO.apartamento(), DTO.bloco());
+    }
+
 
 }
