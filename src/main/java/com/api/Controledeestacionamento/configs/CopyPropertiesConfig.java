79dd20c42b488cd05019872c077bbbb80cd0eab3
luizfellipes <luiz.13.silva@gmail.com>
2024-09-11 15:43:02 -0400

    melhorado o copy properties

--- src/main/java/com/api/Controledeestacionamento/configs/CopyPropertiesConfig.java
+++ src/main/java/com/api/Controledeestacionamento/configs/CopyPropertiesConfig.java
@@ -5,8 +5,7 @@ import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
 
 import java.beans.PropertyDescriptor;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 public abstract class CopyPropertiesConfig {
 
@@ -20,15 +19,11 @@ public abstract class CopyPropertiesConfig {
     }
 
     private static String[] getNullPropertyNames(Object source) {
-        final BeanWrapper src = new BeanWrapperImpl(source);
-        PropertyDescriptor[] pds = src.getPropertyDescriptors();
-        Set<String> emptyNames = new HashSet<>();
-        for (PropertyDescriptor pd : pds) {
-            Object srcValue = src.getPropertyValue(pd.getName());
-            if (srcValue == null) emptyNames.add(pd.getName());
-        }
-        String[] result = new String[emptyNames.size()];
-        return emptyNames.toArray(result);
+        BeanWrapper src = new BeanWrapperImpl(source);
+        return Arrays.stream(src.getPropertyDescriptors())
+                .map(PropertyDescriptor::getName)
+                .filter(name -> src.getPropertyValue(name) == null)
+                .toArray(String[]::new);
     }
 
 }
